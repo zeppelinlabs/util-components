47b68d22d8e5d957c959186be7055896fd6615a1
Over <overquintero@zeppelinlabs.io>
2022-10-25 17:31:15 -0500

    Agrega componente Tags

---
+++ src/components/Tag/Tag.tsx
@@ -0,0 +1,62 @@
+import React from "react"
+import { TagStyled } from "./TagStyled"
+import { ReactComponent as CrossIcon} from "../../assets/icons/cross.svg"
+
+export enum TagSize {
+	Small = "small",
+	Medium = "medium",
+	Large = "large",
+}
+
+export enum TagIconPosition {
+	Left = "row",
+	Right = "row-reverse",
+}
+
+export enum TagTextWeight {
+	Light = "100",
+	Bold = "800",
+}
+
+export type CustomStyles = {
+	borderColor?: string,
+	BackgroundColor?: string,
+	TextColor?: string,
+	TextWeight?: TagTextWeight,
+	size?: TagSize,
+	isCursorPointer?: boolean,
+}
+
+type Props = {
+	children: React.ReactNode,
+	Icon?: {
+		IconSVGComponent: React.FunctionComponent,
+		Position?: TagIconPosition,
+	}
+	customStyles?: CustomStyles,
+	onClick?: () => void,
+	onClickRemove?: () => void,
+}
+
+const Tag = (props: Props) => {
+
+	return <TagStyled.Container
+		IconPosition={props.Icon?.Position}
+		customStyles={props.customStyles}>
+		{props.Icon?.IconSVGComponent
+			&& <TagStyled.CustomIconContainer size={props.customStyles?.size}>
+				<props.Icon.IconSVGComponent/>
+		</TagStyled.CustomIconContainer>}
+
+		{props.children}
+
+		{props.onClickRemove
+			&& <TagStyled.CrossIconContainer
+				size={props.customStyles?.size}
+				onClick={props.onClickRemove}>
+			<CrossIcon />
+		</TagStyled.CrossIconContainer>}
+	</TagStyled.Container>
+}
+
+export default Tag
