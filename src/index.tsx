d25f07718d921be1dbeec71ead6a70f1f7834e95
Over <overquintero@zeppelinlabs.io>
2022-11-04 11:45:57 -0500

    agrega ajustes necesarios para deploy en github

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-process-env */
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
@@ -5,7 +6,7 @@ import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <App />
         </BrowserRouter>
     </React.StrictMode>,
