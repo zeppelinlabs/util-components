6cfc2993d95b55828b6a4baedd29cc87d23472e6
sebastian <sebastian@zeppelinlabs.io>
2022-11-10 15:08:41 -0300

    feat: Add checkbox group example

--- src/examples/Form/CheckboxShowCase/CheckboxGroupShowcase.tsx
+++ src/examples/Form/CheckboxShowCase/CheckboxGroupShowcase.tsx
@@ -23,10 +23,31 @@ const CheckboxGroupShowCase = () => {
         },
     ]
 
+    const options2 = [
+        {
+            key: "option1",
+            children: <h5> JSX Option 1 </h5>,
+        },
+        {
+            key: "option2",
+            children: <h5> JSX Option 2 </h5>,
+        },
+        {
+            key: "option3",
+            children: <h5> JSX Option 3 </h5>,
+        },
+    ]
+
     return <>
         <h3>Checkbox Group</h3>
         <CheckboxGroup
             options={options}
+            keyValue={checkedState.map(o => o.key)}
+            onChange={handleOnChangeCheckboxGroup}
+        />
+        <hr />
+        <CheckboxGroup
+            options={options2}
             keyValue={checkedState.map(o => o.key)}
             onChange={handleOnChangeCheckboxGroup}
         />
