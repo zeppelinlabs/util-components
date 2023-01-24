a9d45c1c688d77eaf1dec2c58f0b465230792b97
sebastian <sebastian@zeppelinlabs.io>
2023-01-24 12:47:43 -0300

    feat: Footer Responsive

--- src/examples/_Layout/Footer/Footer.tsx
+++ src/examples/_Layout/Footer/Footer.tsx
@@ -3,11 +3,10 @@ import { FooterStyled } from "./FooterStyled"
 import ZeppelinLabsLogo from "../../../assets/examples/zeppelin-labs.svg"
 import { Paths } from "../../Paths"
 
-
 const Footer = () => {
 	return <FooterStyled.Wrapper>
 		<FooterStyled.Row>
-			<img src={ZeppelinLabsLogo} alt="Zeppelin Labs" />
+			<img src={ZeppelinLabsLogo} alt="Zeppelin Labs" height={"30px"} />
 			<FooterStyled.ContainerFooterNav>
 				{Paths.zeppelinSocialMedia.map((link, i) => {
 					return <FooterStyled.Link key={i} href={link.link} target="_blank">
--- src/examples/_Layout/Footer/FooterStyled.ts
+++ src/examples/_Layout/Footer/FooterStyled.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 import { UiTokens } from "../../../styles/designTokens/uiTokens"
 import { GlobalStyledHelper } from "../../../styles/globalStyledHelper"
+import { device } from "../../../styles/mediaQueries"
 import { thp } from "../../../styles/themeHelpers"
 
 const Wrapper = styled.footer`
@@ -14,14 +15,25 @@ const Wrapper = styled.footer`
 const Row = styled.section`
 	${GlobalStyledHelper._Row}
 	display: grid;
-	grid-template-columns: 160px 1fr;
-	align-items: center;
-	gap: ${UiTokens.spacing.size16};
+	grid-template-columns: 1fr;
+	@media ${device.desktop} {
+		grid-template-columns: 160px 1fr;
+		gap: ${UiTokens.spacing.size16};
+	}
 `
 const ContainerFooterNav = styled.article`
-	gap: ${UiTokens.spacing.size52};
-	display: flex;
-	justify-content: flex-end;
+	display: grid;
+	gap: ${UiTokens.spacing.size16};
+	grid-template-rows: repeat(5, 1fr);
+	align-items: center;
+	justify-content: center;
+	margin-top: ${UiTokens.spacing.size24};
+	@media ${device.desktop} {
+		gap: ${UiTokens.spacing.size52};
+		justify-content: flex-end;
+		display: flex;
+		margin: 0;
+	}
 `
 
 const Link = styled.a`
@@ -29,6 +41,7 @@ const Link = styled.a`
 	color: ${thp.base.level200._};
 	text-decoration: none;
 	transition: all .3s ease-out;
+	text-align: center;
 	&:hover{
 		color: ${thp.base.level0._};
 	}
