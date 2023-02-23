281606ad3807614166640eba3141cfea884eccfb
sebastian <sebastian@zeppelinlabs.io>
2023-02-23 11:24:15 -0300

    feat: Accordion Variant

--- src/examples/Pages/HowItWorks/Accordion/AccordionStyles.ts
+++ src/examples/Pages/HowItWorks/Accordion/AccordionStyles.ts
@@ -1,13 +1,36 @@
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 import { UiTokens } from "../../../../styles/designTokens/uiTokens"
+import { thp } from "../../../../styles/themeHelpers"
+import { AccordionVariant, CustomAccordionStyles } from "./Accordion"
 
-const AccordionHeader = styled.div`
+
+const getButtonVariant = (variant: AccordionVariant) => {
+    const ButtonByCase = {
+        [AccordionVariant.Primary]: (PrimaryAccordion),
+        [AccordionVariant.Secondary]: (SecondaryAccordion),
+    }
+    return ButtonByCase[variant]
+}
+
+const PrimaryAccordion = () => css`
+    background-color: transparent;
+`
+
+const SecondaryAccordion = () => css`
+    background-color:  ${thp.primary.level300._};
+    color: ${thp.base.level1000._};
+    padding: ${UiTokens.spacing.size2};
+`
+
+const AccordionHeader = styled.div<{ customStyles?: CustomAccordionStyles, }>`
     display: grid;  
     gap: ${UiTokens.spacing.size16};
     align-items: center;
     grid-template-columns: 1fr 16px;
     row-gap: inherit;
     cursor: pointer;
+    ${({ customStyles, }) => (getButtonVariant(customStyles?.accordionVariant
+    || AccordionVariant.Primary))}
 `
 
 const AccordionContent = styled.div<{ hasMarginBottom?: boolean, }>`
