2b387dba22fd3a3ee9a6432061f704c98df012ed
sebastian <sebastian@zeppelinlabs.io>
2023-02-23 15:11:41 -0300

    feat: Footer Logo Redirection

--- src/examples/_Layout/Footer/Footer.tsx
+++ src/examples/_Layout/Footer/Footer.tsx
@@ -2,11 +2,14 @@ import React from "react"
 import { FooterStyled } from "./FooterStyled"
 import ZeppelinLabsLogo from "../../../assets/examples/zeppelin-labs.svg"
 import { Paths } from "../../Paths"
+import { Link } from "react-router-dom"
 
 const Footer = () => {
 	return <FooterStyled.Wrapper>
 		<FooterStyled.Row>
-			<img src={ZeppelinLabsLogo} alt="Zeppelin Labs" />
+			<FooterStyled.Link href={Paths.AboutUs} target="_blank">
+				<img src={ZeppelinLabsLogo} alt="Zeppelin Labs" />
+			</FooterStyled.Link>
 			<FooterStyled.ContainerFooterNav>
 				{Paths.zeppelinSocialMedia.map((link, i) => {
 					return <FooterStyled.Link key={i} href={link.link} target="_blank">
